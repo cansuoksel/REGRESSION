69669036a16ef23053e9f5258a9b137fd5c9efb0
CANSU ÖKSEL <84022695+cansuoksel@users.noreply.github.com>
2024-04-06 02:43:43 +0300

    Add files via upload

---
+++ car_price_prediction.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.linear_model import LinearRegression
+
+data = pd.read_csv("CarPrice_Assignment.csv")
+
+X = data[['horsepower', 'enginesize', 'curbweight']]
+y = data['price']
+
+model = LinearRegression()
+model.fit(X, y)
+
+plt.figure(figsize=(15, 5))
+
+# 'horsepower' özelliğinin grafiği
+plt.subplot(1, 3, 1)
+sns.scatterplot(x='horsepower', y='price', data=data)
+sns.regplot(x='horsepower', y='price', data=data, scatter=False, color='red')
+
+# 'enginesize' özelliğinin grafiği
+plt.subplot(1, 3, 2)
+sns.scatterplot(x='enginesize', y='price', data=data)
+sns.regplot(x='enginesize', y='price', data=data, scatter=False, color='red')
+
+# 'curbweight' özelliğinin grafiği
+plt.subplot(1, 3, 3)
+sns.scatterplot(x='curbweight', y='price', data=data)
+sns.regplot(x='curbweight', y='price', data=data, scatter=False, color='red')
+
+plt.tight_layout()
+plt.show()
